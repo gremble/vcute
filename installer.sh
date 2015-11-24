34a665848d143be4f6ee6d88727270b5716a5f1e
root <root@bucket.psand.net>
2015-11-24 16:53:44 +0000

    added better installer and error checks

--- installer.sh
+++ installer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
 #Installer for vcute
 
+MY_PATH="`dirname \"$0\"`"
 
-echo "This currently does nothing. Sozzers."
+echo "One day this installer will be better..." 
+echo
+
+if [ ! -f /etc/default/vcute ]; then
+   echo "Copying $MY_PATH/etc/default/vcute to /etc/default/vcute ..."
+   cp $MY_PATH/etc/default/vcute /etc/default/vcute
+   echo "Now edit /etc/default/vcute"
+else 
+
+   echo "/etc/default/vcute exists, doing nothing."
+   echo "Edit /etc/default/vcute if VCUTE still isn't working."
+fi
