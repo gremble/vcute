3d5427891c2257aa857ae21a889fc818e090f397
Ben Green <ben.green@psand.net>
2015-11-30 14:41:23 +0000

    added adding a symlink into the installer

--- installer.sh
+++ installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #Installer for vcute
 
-MY_PATH="`dirname \"$0\"`"
+SCRIPT=`realpath -s $0`
+MY_PATH=`dirname $SCRIPT`
 
 echo "One day this installer will be better..." 
 echo
@@ -10,8 +11,15 @@ if [ ! -f /etc/default/vcute ]; then
    echo "Copying $MY_PATH/etc/default/vcute to /etc/default/vcute ..."
    cp $MY_PATH/etc/default/vcute /etc/default/vcute
    echo "Now edit /etc/default/vcute"
-else 
-
+else
    echo "/etc/default/vcute exists, doing nothing."
    echo "Edit /etc/default/vcute if VCUTE still isn't working."
+fi
+
+if [ ! -f /usr/local/bin/vcute ]; then
+   echo "Linking $MY_PATH/usr/bin/vcute <- /usr/local/bin/vcute"
+   ln -s $MY_PATH/usr/bin/vcute /usr/local/bin/vcute
+else
+   echo "Symlink for vcute already there, not doing that again."
+
 fi
